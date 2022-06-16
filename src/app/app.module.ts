be23fea428fc584f0f1f5b8907d1264af38d499a
Priscila <priscila.pineda@hotmail.com>
2022-06-16 17:25:14 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroeComponent } from './Heroes/Heroe/heroe.component';
+import { ListadoComponent } from './Heroes/listado/listado.component';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { ContadorModule } from './contador/contador.Module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
